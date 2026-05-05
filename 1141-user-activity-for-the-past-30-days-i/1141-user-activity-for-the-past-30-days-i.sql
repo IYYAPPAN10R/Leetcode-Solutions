fcc47e72fd93f47719bd42e76beb24bf173b2c95
IYYAPPAN10R <iyyappan281105@gmail.com>
2026-05-05 11:47:21 +0530

    Time: 546 ms (18.79%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,4 @@
+SELECT activity_date as day,count(DISTINCT user_id) as active_users
+from Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27',INTERVAL 29 DAY) AND '2019-07-27'
+group by activity_date
