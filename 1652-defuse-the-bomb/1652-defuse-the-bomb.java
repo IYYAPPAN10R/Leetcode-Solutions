99b9ad78919b4c0e43d9402291b2adf5f56a25dd
IYYAPPAN10R <iyyappan281105@gmail.com>
2026-07-10 14:16:38 +0530

    Time: 2 ms (59.44%), Space: 43.9 MB (47.85%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int result[] = new int[code.length];
+        for(int i = 0;i<code.length;i++){
+            int t2 = k;
+            if(k>0){
+                int temp = 0;
+                int len = i;
+                for(int j = 0;j<k;j++){
+                    len = (len+1)%code.length;
+                    temp += code[len];
+                }
+                result[i] = temp;
+            }
+            else if(k<0){
+                int temp = 0;
+                int len =  i;
+                for(int j = 0;j<-k;j++){
+                    if(len <= 0){
+                        len = code.length-1;
+                        temp += code[len];
+                    }
+                    else{
+                        len--;
+                        temp+= code[len];
+                    }
+                }
+                result[i] = temp;
+            }
+            else{
+                result[i] = 0;
+            }
+        }
+        return result;
+    }
+}
